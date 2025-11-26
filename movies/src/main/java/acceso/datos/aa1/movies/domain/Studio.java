10f98026818bf166e3071dbb34e6965a6a41e738
Cristina <cristina.csevi@gmail.com>
2025-11-26 16:41:57 +0100

    Create domain entities

---
+++ movies/src/main/java/acceso/datos/aa1/movies/domain/Studio.java
@@ -0,0 +1,48 @@
+package acceso.datos.aa1.movies.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "studios")
+
+public class Studio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @NotNull(message = "Name is required")
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @Column
+    @NotNull(message = "Country is required")
+    private String country;
+
+    @Column(name = "foundation_year")
+    @Min(value = 1888, message = "Foundation year must be after 1888")
+    private Integer foundationYear;
+
+    @Column
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private Boolean active;
+
+    @OneToMany(mappedBy = "studio")
+    @JsonBackReference
+    private List<Movie> movies;
+}
