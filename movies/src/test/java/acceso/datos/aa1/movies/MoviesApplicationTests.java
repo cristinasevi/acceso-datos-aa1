081b15f5d72f9a1a876e2dc8db56672be38ede37
Cristina <cristina.csevi@gmail.com>
2025-11-26 15:57:24 +0100

    Initial commit

---
+++ movies/src/test/java/acceso/datos/aa1/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package acceso.datos.aa1.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
