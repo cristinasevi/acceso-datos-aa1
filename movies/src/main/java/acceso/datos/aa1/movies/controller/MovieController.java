97fc00b67d89b6e7bef78bf27dad3bf88c6949cf
Cristina <cristina.csevi@gmail.com>
2026-01-01 13:10:58 +0100

    Create MovieCreateRequest related with programacion-multimedia-aa1

--- movies/src/main/java/acceso/datos/aa1/movies/controller/MovieController.java
+++ movies/src/main/java/acceso/datos/aa1/movies/controller/MovieController.java
@@ -3,6 +3,7 @@ package acceso.datos.aa1.movies.controller;
 import acceso.datos.aa1.movies.domain.Director;
 import acceso.datos.aa1.movies.domain.Movie;
 import acceso.datos.aa1.movies.domain.Studio;
+import acceso.datos.aa1.movies.dto.MovieCreateRequest;
 import acceso.datos.aa1.movies.dto.MovieOutDto;
 import acceso.datos.aa1.movies.dto.MovieUpdateRequest;
 import acceso.datos.aa1.movies.exception.DirectorNotFoundException;
@@ -57,7 +58,36 @@ public class MovieController {
     }
 
     @PostMapping("/movies")
-    public ResponseEntity<Movie> addMovie(@Valid @RequestBody Movie movie) {
+    public ResponseEntity<Movie> addMovie(@Valid @RequestBody MovieCreateRequest movieRequest) {
+        Movie movie = new Movie();
+        movie.setTitle(movieRequest.getTitle());
+        movie.setSynopsis(movieRequest.getSynopsis());
+        movie.setReleaseDate(movieRequest.getReleaseDate());
+        movie.setDuration(movieRequest.getDuration());
+        movie.setGenre(movieRequest.getGenre());
+        movie.setImageUrl(movieRequest.getImageUrl());
+        movie.setAverageRating(movieRequest.getAverageRating());
+
+        if (movieRequest.getStudioId() != null && movieRequest.getStudioId() > 0) {
+            try {
+                Studio studio = studioRepository.findById(movieRequest.getStudioId())
+                        .orElseThrow(StudioNotFoundException::new);
+                movie.setStudio(studio);
+            } catch (StudioNotFoundException e) {
+                return ResponseEntity.badRequest().build();
+            }
+        }
+
+        if (movieRequest.getDirectorId() != null && movieRequest.getDirectorId() > 0) {
+            try {
+                Director director = directorRepository.findById(movieRequest.getDirectorId())
+                        .orElseThrow(DirectorNotFoundException::new);
+                movie.setDirector(director);
+            } catch (DirectorNotFoundException e) {
+                return ResponseEntity.badRequest().build();
+            }
+        }
+
         Movie newMovie = movieService.add(movie);
         return new ResponseEntity<>(newMovie, HttpStatus.CREATED);
     }
---
+++ movies/src/main/java/acceso/datos/aa1/movies/dto/MovieCreateRequest.java
@@ -0,0 +1,24 @@
+package acceso.datos.aa1.movies.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieCreateRequest {
+    private String title;
+    private String synopsis;
+    private LocalDate releaseDate;
+    private Integer duration;
+    private String genre;
+    private String imageUrl;
+    private Float averageRating;
+    private Long studioId;
+    private Long directorId;
+}
